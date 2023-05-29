a7cea7be8dff488815050a174fcf12ef42ba8d00
ayushete2005 <ayushete2005@gmail.com>
2023-05-29 15:14:43 +0530

    first commit

---
+++ client/src/components/ConnectWallet.jsx
@@ -0,0 +1,33 @@
+import { ethers } from 'ethers';
+import React from 'react';
+
+const ConnectWallet = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account);
+    }
+
+    return (
+        <nav>
+            {account ? (
+                <button
+                    type="button"
+                    className='nav__connect'
+                >
+                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button
+                    type="button"
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
+            )}
+        </nav>
+    );
+}
+
+export default ConnectWallet;
